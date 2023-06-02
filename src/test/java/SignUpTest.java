ccc38120e3af1ad13a50be0a05fa97e43f69fe91
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-06-02 14:56:27 +0300

    Delete import

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -32,4 +32,36 @@ public class SignUpTest {
 
     }
 
+
+
+    @Test
+    public void zipCodeShouldAccept4Digits(){
+        //1. перейти на https://www.sharelane.com/cgi-bin/register.py
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get ("https://www.sharelane.com/cgi-bin/register.py");
+        // 2. Ввести - 1234
+        WebElement zipCodeInput =  driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("1234");
+        // 3. Нажать на кнопку Continue
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+        //4. убедиться что выводится ошибка на экране
+        WebElement errorMessage = driver.findElement(By.cssSelector("[class=error_message]"));
+        errorMessage.getText();
+        String text = errorMessage.getText();
+        Assert.assertEquals(text,"Oops, error on page. ZIP code should have 5 digits","Сообщение не отображается");
+        //5. закрыть браузер
+        driver.quit();
+
+    }
 }
+
+
+
+
+
+
+
+
